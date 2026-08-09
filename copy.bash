9a013c2094808aeec09ca4201f9812098d791f61
Reed Fu <rfugithub@outlook.com>
2026-08-08 22:14:07 -0400

    copy shell script

---
+++ copy.bash
@@ -0,0 +1,11 @@
+rm -rf 3po
+mkdir 3po
+mkdir 3po/_tools
+mkdir 3po/_dialogs
+mkdir 3po/layouts
+cd 3po
+cp /Users/reedfu/Skynet/threepio/threepio.py .
+cp /Users/reedfu/Skynet/threepio/_tools/*.py _tools
+cp /Users/reedfu/Skynet/threepio/_dialogs/*.py _dialogs
+cp /Users/reedfu/Skynet/threepio/layouts/*.py layouts
+cp /Users/reedfu/Skynet/threepio/layouts/*.ui layouts
